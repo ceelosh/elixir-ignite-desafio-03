a3961fc779cd6350ffee6a7da9d16695d0fceea4
Marcelo Muniz da Luz <ma_celinho18@hotmail.com>
2021-09-11 18:20:52 -0300

    starting

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,57 @@
 defmodule GenReport do
+  alias GenReport.Parser
+  @workers [
+    "cleiton",
+    "daniele",
+    "danilo",
+    "diego",
+    "giuliano",
+    "jakeliny",
+    "joseph",
+    "mayk",
+    "rafael",
+    "vinicius"
+  ]
 
-  def build do
-    # TO DO
+  @months [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
+
+  def build(filename) do
+    filename
+    |> Parser.parse_line()
+    |> Enum.reduce(report(), fn line, report -> sum_values(line, report) end)
   end
+
+  def build(_filename), do: {:error, "Insira o nome de um arquivo"}
+
+  defp sum_values([name, quantidade, _dia, mes, ano], %{"all_hours" => all_hours, "hours_per_month" => hours_per_month, "hours_per_year" => hours_per_year} = report) do
+    all_hours = Map.put(all_hours, name, report[name] + quantidade)
+    hours_per_month = Map.put(hours_per_month, name, report[name][mes] + quantidade)
+    hours_per_year = Map.put(hours_per_year, name, report[name][ano] + quantidade)
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+    |> IO.inspect()
+  end
+
+  defp report() do
+    all_hours = Enum.into(@workers, %{}, &{&1, 0})
+    hours_per_month = Enum.into(@months, %{}, &{&1, 0})
+    hours_per_year = Enum.into(@workers, %{}, &{&1, 0})
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp build_report(all_hours, hours_per_month, hours_per_year), do: %{"all_hours" => all_hours, "hours_per_month" => hours_per_month, "hours_per_year" => hours_per_year}
 end
---
+++ lib/parser.ex
@@ -0,0 +1,35 @@
+defmodule GenReport.Parser do
+
+  def parse_file(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Stream.map(&parse_line/1)
+  end
+
+  def parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(1, &String.to_integer/1)
+    |> List.update_at(2, &String.to_integer/1)
+    |> List.update_at(3, &to_month(String.to_integer(&1)))
+    |> List.update_at(4, &String.to_integer/1)
+  end
+
+  defp to_month(num_of_month) when num_of_month >= 1 and num_of_month <= 12 do
+    case num_of_month do
+      1 -> "janeiro"
+      2 -> "fevereiro"
+      3 -> "março"
+      4 -> "abril"
+      5 -> "maio"
+      6 -> "junho"
+      7 -> "julho"
+      8 -> "agosto"
+      9 -> "setembro"
+      10 -> "outubro"
+      11 -> "novembro"
+      12 -> "dezembro"
+    end
+  end
+end
